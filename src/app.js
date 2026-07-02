dadda0b523928e6265265665512ffb29db5d4b69
karan-57 <porcheixii@gmail.com>
2026-07-02 12:14:02 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const authRoutes = require('./routes/auth.routes')
 const accountRoutes = require('./routes/account.routes')
@@ -7,6 +8,10 @@ const transactionRoutes = require('./routes/transaction.routes')
 
 const app = express()
 
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser())
 
